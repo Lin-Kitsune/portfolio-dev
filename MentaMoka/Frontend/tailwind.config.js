2ef44bc53e26edc448a857e9d516d38847eeae1d
Lin-Kitsune <kitsunelin567@gmail.com>
2025-03-13 20:20:14 -0300

    feat: se mejoro la estetica de la pagina

--- MentaMoka/Frontend/tailwind.config.js
+++ MentaMoka/Frontend/tailwind.config.js
@@ -14,9 +14,14 @@ module.exports = {
           cream: '#FAF9F7',            // alias
           brown: '#9C6F48',            // alias
           beigeDark: '#EBDAC6',        // alias
-          greenSoft: '#A8BBA1',        // alias
-          greenDark: '#81977B'         // inventado para el hover
-      }
+          greenSoft: '#bfd992',        // alias
+          greenDark: '#81977B',         // inventado para el hover
+          brownDark: '#3A2A25'
+      },
+      container: {
+        center: true, // Centrar contenedor globalmente
+        padding: "1rem", // Añadir padding para evitar desbordes en móviles
+      },
       
       }
     },
