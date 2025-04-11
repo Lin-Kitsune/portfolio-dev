abf14cee72fc37e4c2bf26ca92f2b96f2f4dab53
Lin-Kitsune <kitsunelin567@gmail.com>
2025-04-11 19:20:26 -0400

    Delete MentaMoka directory
    Se realizo el cambio de nombre al nombre original

--- MentaMoka/Frontend/src/app/models/inventory.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Ingredient {
-  id: string;
-  name: string;
-  stock: number;
-  quantity: number;
-  unit: 'ml' | 'g' | 'units';
-  image_url?: string; // 🔥 Opcional pero recomendado
-  type: 'milk' | 'sugar' | 'other'; 
-}
